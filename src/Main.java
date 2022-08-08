21ab3b0d9eae01711898fb09c76c451ff9aa9635
User <mirgulabdinazarova@gmail.com>
2022-08-08 19:02:33 +0600

    Birinchi ozgoruu3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-     /* Computer computer=new Computer();
-        computer. model="Lenovo";
-        computer.color="black";
-        computer.price=50000;
-        computer.ram=8;
-        computer.ssd=256;
-        computer.weight=2.5;
-        System.out.println(computer);*/
-
+        Balyk balyk=new Balyk();
+        balyk.setName("Petushok");
+        balyk.setColor("Krasnyi");
+        balyk.setAge(1);
+        System.out.println(balyk.getName());
+        System.out.println(balyk.getColor());
+        System.out.println(balyk.getAge());
+        System.out.println("------------------------");
+        Popugai popugai=new Popugai();
+        popugai.setName("Kesha");
+        popugai.setColor("Belyi");
+        popugai.setAge(3);
+        System.out.println(popugai.getName());
+        System.out.println(popugai.getColor());
+        System.out.println(popugai.getAge());
+        System.out.println("---------------------------");
+        It it=new It();
+        it.setName("Rex");
+        it.setColor("Chernyi");
+        it.setAge(2);
+        System.out.println(it.getName());
+        System.out.println(it.getColor());
+        System.out.println(it.getAge());
+        System.out.println("---------------------------");
+        Myshyk myshyk=new Myshyk();
+        myshyk.setName("Masha");
+        myshyk.setColor("Seryi");
+        myshyk.setAge(4);
+        System.out.println(myshyk.getName());
+        System.out.println(myshyk.getColor());
+        System.out.println(myshyk.getAge());
 
     }
+
     }
+
+
---
+++ src/Popugai.java
@@ -0,0 +1,25 @@
+public class Popugai {
+
+     private String name;
+     private String color;
+     private int age;
+
+     void setName(String aty){
+          name=aty;
+     }
+     String getName(){
+          return name;
+     }
+     void setColor(String ony){
+          color=ony;
+     }
+     String getColor(){
+          return color;
+     }
+     void setAge(int jashy){
+          age=jashy;
+     }
+     int getAge(){
+          return age;
+     }
+}
